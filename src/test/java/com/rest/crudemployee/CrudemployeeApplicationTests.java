52cb151ff38e1175c818785b438a01c9618f7d0f
tinkalkumar007 <tinkal.nitmz@gmail.com>
2024-05-20 16:31:46 +0530

    crud operation using jparepository and restcontroller

---
+++ src/test/java/com/rest/crudemployee/CrudemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.crudemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
